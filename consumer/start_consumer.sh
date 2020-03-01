c592359813ad29d562bf59c8b4aae9fe63011faa
vagrant <vagrant@vagrant.vm>
2020-03-01 18:51:32 +0000

    Moved VNC port to extra arguments

--- consumer/start_consumer.sh
+++ consumer/start_consumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -itd -p 80:80 --network network1 --name=consumer -e "RTSP_URL=rtsp://streamer:8090/test" akshshar/vlc-consumer
+docker run --rm -itd -p 80:80 --network network1 --name=consumer -e "RTSP_URL=rtsp://streamer:8090/test" -e "VNC_PORT=80" akshshar/vlc-consumer
